3c621d6edef7dd12dbdc9a62a67608ceeb160df7
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-05 15:20:29 +0000

    为data/movie添加了double rank 修改data/star的rank int为double

---  moviecubes/src/main/Common/Data/Movie.cs
+++  moviecubes/src/main/Common/Data/Movie.cs
@@ -14,45 +14,47 @@ namespace MovieCube.Common.Data
         /// <summary>
         /// 名称
         /// </summary>
-        public string Name{get;set;}
+        public string Name { get; set; }
 
         /// <summary>
         /// 别名
         /// </summary>
-        public string Alia{get;set;}
+        public string Alia { get; set; }
 
         /// <summary>
         /// 导演
         /// </summary>
-        public string[] Directors{get;set;}
+        public string[] Directors { get; set; }
 
         /// <summary>
         /// 编剧
         /// </summary>
-        public string[] Writers{get;set;}
+        public string[] Writers { get; set; }
 
         /// <summary>
         /// 演员（有个关系表，演员ID，演员的角色（主演之类的））
         /// </summary>
-        public string[] Actors{get;set;}
-
+        public string[] Actors { get; set; }
 
         /// <summary>
         /// 类型
         /// </summary>
-        public string Type{get;set;}
+        public string Type { get; set; }
         
 
         /// <summary>
         /// 地区
         /// </summary>
-        public string Area{get;set;}
+        public string Area { get; set; }
 
         /// <summary>
         /// 语言
         /// </summary>
-        public string Language{get;set;}
+        public string Language { get; set; }
 
-
+        /// <summary>
+        /// 分值
+        /// </summary>
+        public double Rank { get; set; }
     }
 }
