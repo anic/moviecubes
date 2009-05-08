99441f5215ea078adc4bfe97e5ddcec5f599a1ef
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-08 13:58:46 +0000

    添加了三个表的访问类

---
+++ trunk/ moviecubes/src/main/RelationalDataAccess/MovieAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MovieCube.RelationalDataAccess
+{
+    public class MovieAccess
+    {
+        public static DataSet GetMovieByName(string name)
+        {
+            DataSet ds = new DataSet();
+
+            using (MySqlConnection conn = new MySqlConnection(
+                ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("sp_GetMovieByName", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("name_", MySqlDbType.MediumText);
+                cmd.Parameters["name_"].Direction = ParameterDirection.Input;
+                cmd.Parameters["name_"].Value = name;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(ds);
+
+                conn.Close();
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ trunk/ moviecubes/src/main/RelationalDataAccess/StarAccess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieCube.RelationalDataAccess
+{
+    public class StarAccess
+    {
+    }
+}
