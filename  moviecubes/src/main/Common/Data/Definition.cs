6c703fbbbb4d6df8d14d479e95e2785b21ed03a3
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-09 07:35:25 +0000

    添加了Definition类，定义一些常量

---
+++  moviecubes/src/main/Common/Data/Definition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieCube.Common.Data
+{
+    public class Definition
+    {
+        //节点搜索最大深度
+        public const int Max_Node_Layer = 3;
+        
+        //周围节点最大个数
+        public const int Max_Surround_Node_Num = 5;
+
+
+
+        public const string Role_Director = "导演";
+        public const string Role_Writer = "编剧";
+        public const string Role_Actor = "主演";
+    }
+}
