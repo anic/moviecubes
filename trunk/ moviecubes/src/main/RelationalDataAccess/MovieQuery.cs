99d235733af583c090958b219151387817d6408a
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-09 07:37:12 +0000

    更新RelationalDataAccess。实现了imoviequery和istarquery的各一个接口 目前只返回两层数据

---
+++ trunk/ moviecubes/src/main/RelationalDataAccess/MovieQuery.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+using MovieCube.Common;
+using MovieCube.Common.Data;
+using MovieCube.Common.Interface;
+
+namespace MovieCube.RelationalDataAccess
+{
+    public class MovieQuery: IMovieQuery
+    {
+        #region IMovieQuery 成员
+
+        public List<Movie> QueryMovieByName(string name)
+        {
+            return GetMovieByName(name);
+        }
+
+        public List<Movie> QueryMovieByActor(string actorName)
+        {
+            return null;
+        }
+
+        #endregion
+
+        private List<Movie> GetMovieByName(string name)
+        {
+            List<Movie> resultMovies = new List<Movie>();
+            DataSet ds = MovieAccess.GetInfoByMovieName(name);
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++ )
+            {
+                DataRow dr = ds.Tables[0].Rows[i];
+
+                int id = Convert.ToInt32(dr["movieID"].ToString());
+
+                Star star = new Star();
+                star.ID = Convert.ToInt32(dr["starID"].ToString());
+                star.Name = dr["starName"].ToString();
+                star.Rank = Convert.ToDouble(dr["starRank"].ToString());
+                star.Image = dr["starImage"].ToString();
+
+                string staralias = dr["starAlia"].ToString();
+                if(staralias != "")
+                    Util.ProcessStringItem(staralias, star.Alias);
+
+                string role = dr["role"].ToString();
+
+                Movie movie = Util.FindMovie(id, resultMovies);
+                if (movie == null)
+                {
+                    movie = new Movie();
+                    movie.ID = id;
+                    movie.Name = dr["movieName"].ToString();
+                    movie.Image = dr["movieImage"].ToString();
+                    movie.Language = dr["movieLanguage"].ToString();
+                    movie.Area = dr["movieArea"].ToString();
+                    movie.Time = dr["movieTime"].ToString();
+                    movie.Rank = Convert.ToDouble(dr["movieRank"].ToString());
+
+                    string movieType = dr["movieType"].ToString();
+                    if (movieType != "")
+                        Util.ProcessStringItem(movieType, movie.Type);
+
+                    string movieAlias = dr["movieAlia"].ToString();
+                    if (movieAlias != "")
+                        Util.ProcessStringItem(movieAlias, movie.Alias);
+
+                    movie.AddStars(star, role);
+                    resultMovies.Add(movie);
+                }
+                else
+                    movie.AddStars(star, role);
+            }
+
+            return resultMovies;
+        }
+    }
+}
---
+++ trunk/ moviecubes/src/main/RelationalDataAccess/StarQuery.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MovieCube.Common;
+using MovieCube.Common.Data;
+using MovieCube.Common.Interface;
+using System.Data;
+
+namespace MovieCube.RelationalDataAccess
+{
+    public class StarQuery : IStarQuery
+    {
+
+        #region IStarQuery 成员
+
+        public List<Star> QueryStarByName(string name)
+        {
+            return GetStarByName(name);
+        }
+
+        public List<Star> QueryStarByMovie(string movieName)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+        private List<Star> GetStarByName(string name)
+        {
+            List<Star> resultStars = new List<Star>();
+            DataSet ds = StarAccess.GetInfoByStarName(name);
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                DataRow dr = ds.Tables[0].Rows[i];
+
+                int id = Convert.ToInt32(dr["starID"].ToString());
+
+                Movie movie = new Movie();
+                movie.ID = Convert.ToInt32(dr["movieID"].ToString());
+                movie.Name = dr["movieName"].ToString();
+                movie.Image = dr["movieImage"].ToString();
+                movie.Language = dr["movieLanguage"].ToString();
+                movie.Area = dr["movieArea"].ToString();
+                movie.Time = dr["movieTime"].ToString();
+                movie.Rank = Convert.ToDouble(dr["movieRank"].ToString());
+
+                string movieType = dr["movieType"].ToString();
+                if (movieType != "")
+                    Util.ProcessStringItem(movieType, movie.Type);
+
+                string movieAlias = dr["movieAlia"].ToString();
+                if (movieAlias != "")
+                    Util.ProcessStringItem(movieAlias, movie.Alias);
+
+                string role = dr["role"].ToString();
+
+                Star star = Util.FindStar(id, resultStars);
+                if (star == null)
+                {
+                    star = new Star();
+                    star.ID = id;
+                    star.Name = dr["starName"].ToString();
+                    star.Rank = Convert.ToDouble(dr["starRank"].ToString());
+                    star.Image = dr["starImage"].ToString();
+
+                    string staralias = dr["starAlia"].ToString();
+                    if (staralias != "")
+                        Util.ProcessStringItem(staralias, star.Alias);
+
+                    star.AddMovies(movie, role);
+                    resultStars.Add(star);
+                }
+                else
+                    star.AddMovies(movie, role);
+            }
+
+            return resultStars;
+        }
+    }
+}
