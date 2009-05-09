c7138b76bbcd8bf1a140f01c2a3a8e02237f36aa
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-09 07:37:12 +0000

    更新RelationalDataAccess。实现了imoviequery和istarquery的各一个接口 目前只返回两层数据

---  moviecubes/src/main/RelationalDataAccess/MovieAccess.cs
+++  moviecubes/src/main/RelationalDataAccess/MovieAccess.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Configuration;
-
 using System.Data;
 using MySql.Data.MySqlClient;
 
@@ -10,6 +9,71 @@ namespace MovieCube.RelationalDataAccess
 {
     public class MovieAccess
     {
+        /// <summary>
+        /// 根据movieName获得movie信息和movie的star信息
+        /// </summary>
+        /// <param name="movieName"></param>
+        /// <returns></returns>
+        public static DataSet GetInfoByMovieName(string movieName)
+        {
+            DataSet ds = new DataSet();
+
+            using (MySqlConnection conn = new MySqlConnection(
+                ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("sp_GetInfoByMovieName", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("movieName_", MySqlDbType.MediumText);
+                cmd.Parameters["movieName_"].Direction = ParameterDirection.Input;
+                cmd.Parameters["movieName_"].Value = movieName;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(ds);
+
+                conn.Close();
+            }
+
+            return ds;
+        }
+
+        /// <summary>
+        /// 根据id获得movie
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static DataSet GetMovieByID(int id)
+        {
+            DataSet ds = new DataSet();
+
+            using (MySqlConnection conn = new MySqlConnection(
+                ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("sp_GetMovieByID", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("id_", MySqlDbType.Int32);
+                cmd.Parameters["id_"].Direction = ParameterDirection.Input;
+                cmd.Parameters["id_"].Value = id;
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(ds);
+
+                conn.Close();
+            }
+
+            return ds;
+        }
+
+        /// <summary>
+        /// 根据name获得movie
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public static DataSet GetMovieByName(string name)
         {
             DataSet ds = new DataSet();
