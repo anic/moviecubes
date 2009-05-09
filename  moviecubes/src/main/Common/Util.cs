53508c51fa28f2a93303241935eb857a6dc9fc4b
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-09 07:36:09 +0000

    添加了util类，放一些公用的处理函数

---
+++  moviecubes/src/main/Common/Util.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MovieCube.Common.Data;
+
+namespace MovieCube.Common
+{
+    public class Util
+    {
+        /// <summary>
+        /// 将用" , "隔开的数据添加到result中
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="result"></param>
+        public static void ProcessStringItem(string s, List<string> result)
+        {
+            string[] resultArray = s.Split(new string[] { " , " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                if (resultArray[i] != "")
+                    result.Add(resultArray[i]);
+            }
+        }
+
+        /// <summary>
+        /// 给定id在List<Movie>中查找movie
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="movieList"></param>
+        /// <returns>返回找到的movie或者null</returns>
+        public static Movie FindMovie(int id, List<Movie> movieList)
+        {
+            for (int i = 0; i < movieList.Count; i++)
+            {
+                if (movieList[i].ID == id)
+                    return movieList[i];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 给定id在List<Star>中查找star
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="starList"></param>
+        /// <returns>返回找到的star或者null</returns>
+        public static Star FindStar(int id, List<Star> starList)
+        {
+            for(int i=0; i< starList.Count; i++)
+            {
+                if (starList[i].ID == id)
+                    return starList[i];
+            }
+            return null;
+        }
+    }
+}
