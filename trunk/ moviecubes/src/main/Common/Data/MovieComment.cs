c0dcc708ed7dced46af88c676803e0396e3d9913
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-28 13:54:50 +0000

    评论数据结构

--- trunk/ moviecubes/src/main/Common/Data/MovieComment.cs
+++ trunk/ moviecubes/src/main/Common/Data/MovieComment.cs
@@ -10,7 +10,8 @@ namespace MovieCube.Common.Data
         public string AuthorIcon { get; set; }
         public string AuthorName { get; set; }
         public string Title { get; set; }
-        public string UpdatedDate { get; set; }
+        public string PublishDate { get; set; }
+        public string UpdateDate { get; set; }
         public string Summary { get; set; }
     }
 }
