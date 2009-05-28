ce3be74e978f471c819f5b52cce15c4b17103c99
wangw04 <wangw04@8df53be0-358c-11de-8e0c-53a27eea117e>
2009-05-28 13:54:28 +0000

    doubancomment实现

--- trunk/ moviecubes/src/main/RelationalDataAccess/DoubanComment.cs
+++ trunk/ moviecubes/src/main/RelationalDataAccess/DoubanComment.cs
@@ -19,73 +19,88 @@ namespace MovieCube.RelationalDataAccess
 
         public List<MovieComment> GetMovieCommentByName(string name)
         {
-            GetMovieCommentXml(name);
-            return null;
+            //GetMovieCommentXml(name);
+            return GetMovieComment(name);
         }
 
         #endregion
 
-        private string GetMovieCommentXml(string name)
+        private List<MovieComment> GetMovieComment(string name)
         {
+            List<MovieComment> result = new List<MovieComment>();
             string id = GetMovieIDByName(name);
             if(id != "")
             {
                 string url = string.Format("http://api.douban.com/movie/subject/{0}/reviews?start-index=1&max-results={1}", id, Definition.Max_Comment_Num);
 
-                // xmlStream = GetPages(url);
-                //XmlDocument xmldoc = new XmlDocument();
-                //if (xmlStream != null)
-                //{
-                //    xmldoc.Load(xmlStream);
-                //}
+                Uri uri = null;
+                HttpWebRequest request = null;
+                WebResponse response = null;
+                Stream stream = null;
+                StreamReader reader = null;
+
+                try
+                {
+                    url = HttpUtility.HtmlEncode(url);
+                    uri = new Uri(url);
+
+                    request = (HttpWebRequest)WebRequest.Create(uri);
+
+                    response = request.GetResponse();
+                    stream = response.GetResponseStream();
+
+                    XmlDocument xmldoc = new XmlDocument();
+
+                    xmldoc.Load(stream);
+
+                    XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmldoc.NameTable);
+                    nsmgr.AddNamespace("aa", "http://www.w3.org/2005/Atom");
+                    XmlNodeList nodeList = xmldoc.SelectNodes("//aa:entry", nsmgr);
+
+                    foreach (XmlNode xn in nodeList)
+                    {
+                        MovieComment mc = new MovieComment();
+                        mc.Title = xn.SelectSingleNode("aa:title", nsmgr).InnerText;
+                        mc.PublishDate = xn.SelectSingleNode("aa:published", nsmgr).InnerText;
+                        mc.UpdateDate = xn.SelectSingleNode("aa:updated", nsmgr).InnerText;
+                        mc.Summary = xn.SelectSingleNode("aa:summary", nsmgr).InnerText;
+
+                        XmlNode authorNode = xn.SelectSingleNode("aa:author", nsmgr);
+
+                        mc.AuthorLink = authorNode.SelectSingleNode("aa:link[@rel='self']", nsmgr).Attributes["href"].Value;
+                        mc.AuthorIcon = authorNode.SelectSingleNode("aa:link[@rel='icon']", nsmgr).Attributes["href"].Value;
+                        mc.AuthorName = authorNode.SelectSingleNode("aa:name", nsmgr).InnerText;
+
+                        result.Add(mc);
+                    }
+                    return result;
+
+                }
+                catch (Exception e)
+                {
+
+                }
+                finally
+                {
+                    if (reader != null) reader.Close();
+                    if (stream != null) stream.Close();
+                    if (response != null) response.Close();
+                }
             }
             
-            return "";
+            return result;
         }
 
         private string GetMovieIDByName(string name)
         {
-            string url = string.Format("http://api.douban.com/movie/subjects?tag={0}&start-index=1&max-results=2", name);
-
-            //string xmlstring = GetPages(url);
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(url));
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string url = string.Format("http://api.douban.com/movie/subjects?tag={0}&start-index=1&max-results=1", name);
             
-            XmlDocument xmldoc = new XmlDocument();
-            xmldoc.Load(response.GetResponseStream());
-            System.Console.WriteLine(xmldoc.InnerXml);
-            //if (xmlstring != "")
-            {
-                //xmldoc.LoadXml(xmlstring);
-
-                
-                XmlNode root = xmldoc.ChildNodes[1];
-                XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmldoc.NameTable);
-                nsmgr.AddNamespace("aa", "http://www.w3.org/2005/Atom");
-                XmlNodeList nodeList = xmldoc.SelectNodes("//aa:entry", nsmgr);
-
-
-                foreach(XmlNode xn in root.ChildNodes)
-                {
-                    if (xn.Name == "entry")
-                    {
-                        XmlNode idnode = xn.SelectSingleNode(@"/id");
-                        XmlNode titlenode = xn.SelectSingleNode("/title");
-                    }
-                }
-            }
-
-            return "";
-        }
-
-        private static string GetPages(string url)
-        {
             Uri uri = null;
             HttpWebRequest request = null;
             WebResponse response = null;
             Stream stream = null;
             StreamReader reader = null;
+
             try
             {
                 url = HttpUtility.HtmlEncode(url);
@@ -96,25 +111,32 @@ namespace MovieCube.RelationalDataAccess
                 response = request.GetResponse();
                 stream = response.GetResponseStream();
 
-                
-                string buffer = "";
-                string line;
+                XmlDocument xmldoc = new XmlDocument();
 
-                reader = new StreamReader(stream, System.Text.Encoding.UTF8);
+                xmldoc.Load(stream);
 
-                while ((line = reader.ReadLine()) != null)
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmldoc.NameTable);
+                nsmgr.AddNamespace("aa", "http://www.w3.org/2005/Atom");
+                XmlNodeList nodeList = xmldoc.SelectNodes("//aa:entry", nsmgr);
+
+                foreach (XmlNode xn in nodeList)
                 {
-                    buffer += line + "\r\n";
+                    XmlNode idnode = xn.SelectSingleNode("aa:id", nsmgr);
+                    XmlNode titlenode = xn.SelectSingleNode("aa:title", nsmgr);
+
+                    //http://api.douban.com/movie/subject/1424406
+                    string idstring = idnode.InnerText;
+                    int pos = idstring.LastIndexOf('/');
+                    string id = idstring.Substring(pos + 1);
+                    string title = titlenode.InnerText;
+
+                    return id;
                 }
-                return buffer;
+
             }
-            catch (WebException e)
+            catch (Exception e)
             {
-                //System.Console.WriteLine("下载失败，错误：" + e);
-            }
-            catch (IOException e)
-            {
-                //System.Console.WriteLine("下载失败，错误：" + e);
+ 
             }
             finally
             {
@@ -122,7 +144,8 @@ namespace MovieCube.RelationalDataAccess
                 if (stream != null) stream.Close();
                 if (response != null) response.Close();
             }
-            return null;
+
+            return "";
         }
     }
 }
